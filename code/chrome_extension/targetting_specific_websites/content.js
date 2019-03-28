6808c1bc769cf3657f4f43ce6cf5d41447a788de
cyrus <cyrus7@hotmail.fr>
2019-03-28 10:32:59 -0400

    2019.03.28 | Images + Logs

--- code/chrome_extension/targetting_specific_websites/content.js
+++ code/chrome_extension/targetting_specific_websites/content.js
@@ -13,11 +13,13 @@ const sentencesForSpecialRenderings = [
   "Special: Ye shall know the truth and the truth will make you free.",
 ]
 
-if(window.location.host === "www.gooogle.com"){
-  document.querySelectorAll('h2').forEach(function(element) {
-      element.innerText = sentencesForSocialRenderings[Math.floor(Math.random()*sentencesForSocialRenderings.length)];
+if(window.location.host === "duckduckgo.com"){
+  document.querySelectorAll('.result__title').forEach(function(element) {
+    element.innerText = sentencesForSocialRenderings[Math.floor(Math.random()*sentencesForSocialRenderings.length)];
   });
 }
+
+
 if(window.location.host === "www.nytimes.com"){
   document.querySelectorAll('h2').forEach(function(element) {
       element.innerText = sentencesForSpecialRenderings[Math.floor(Math.random()*sentencesForSpecialRenderings.length)];
--- logs.md
+++ logs.md
@@ -521,6 +521,4 @@ Going back to `chrome extensions` now. I created a folder called `code` and a su
 
    ![](<https://raw.githubusercontent.com/cyruslk/franklin-ford-bot/master/process_files/Capture%20d%E2%80%99%C3%A9cran%2C%20le%202019-03-27%20%C3%A0%2018.31.47.png>) 
 
-3. **targetting_specific_websites** 
-
-   More on this soon.
+3. **targetting_specific_websites**: here, the idea would be target specific websites and inject content in their webpages. e.g: if the user goes to www.duckduckgo.com, content A from Ford appears; if he goes to www.nytimes.com, content B from Ford apprears - and so on; we define the websites the code will *listen* to and the code does the rest.
