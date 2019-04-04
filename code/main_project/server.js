34b7f701e987ae7124bbf25c6029c1bf3f1ff242
cyrus <cyrus7@hotmail.fr>
2019-04-04 16:29:29 -0400

    fs -> filepath

--- code/main_project/server.js
+++ code/main_project/server.js
@@ -4,23 +4,35 @@ const http = require('http').Server(app);
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 var fs = require('fs');
+const textFiles = './textFiles/';
 const axios = require("axios");
 var config = require('./config.js');
 
 const spreadsheetURL = config.preFix+config.sheetID+config.postFix;
 
-axios.get(spreadsheetURL)
-  .then(function (response) {
-    // console.log(response.data.feed.entry);
-    // Then here, get a random element of the data.
-    // Once a random element will be picked, run the OCR and go pick a random string.
-    // Then, Tweet it
-    // Then, send it to the Client? Using Socket? Using a simple server setup
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+function runTheBot(){
+  axios.get(spreadsheetURL)
+    .then(function (response) {
 
-app.listen(port, () => {
-  console.log('listening on port ' + port)
-});
+      let arrayOfData = response.data.feed.entry;
+      let length = arrayOfData.length;
+      let randomItem = arrayOfData[Math.floor(Math.random()*arrayOfData.length)];
+      let pickenFile = randomItem.gsx$source.$t;
+      let filePath = textFiles + pickenFile;
+
+      // read now
+      // text or PDF
+
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+  app.listen(port, () => {
+    console.log('listening on port ' + port)
+  });
+}
+
+
+runTheBot();
