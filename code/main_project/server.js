2b8f4b94f5d6ada698b87ea418a169439a183d05
cyrus <cyrus7@hotmail.fr>
2019-04-08 10:56:59 -0400

    2019.04.09 | Adding logs + source-code

--- code/main_project/server.js
+++ code/main_project/server.js
@@ -4,7 +4,6 @@ const http = require('http').Server(app);
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 var fs = require('fs');
-const textFiles = 'textFiles';
 const axios = require("axios");
 var config = require('./config.js');
 
@@ -17,11 +16,31 @@ function runTheBot(){
       let arrayOfData = response.data.feed.entry;
       let length = arrayOfData.length;
       let randomItem = arrayOfData[Math.floor(Math.random()*arrayOfData.length)];
-      let pickenFile = randomItem.gsx$source.$t;
-      let filePath = textFiles + pickenFile;
 
-      // read now
-      // text or PDF
+      let randomItemFormatted = {
+        date: randomItem.gsx$name.$t,
+        physicalLocation: randomItem.gsx$physicallocation.$t,
+        date: randomItem.gsx$date.$t,
+        note: randomItem.gsx$note.$t,
+        homeOfficeTravel: randomItem.gsx$homeofficetravel.$t
+      }
+
+      let pickenFile = randomItem.gsx$source.$t;
+      let filePath = `textFiles/${pickenFile}.txt`;
+
+      fs.readFile(filePath, function read(err, data) {
+        if(err){
+          console.log("show the err here", err);
+        }else{
+          let stringsArray = data.toString('utf8').split(".");
+          let randomString = stringsArray[Math.floor(Math.random()*stringsArray.length)];
+
+          // console.log(randomString);
+          // console.log(pickenFile);
+          // console.log(randomItemFormatted);
+
+        }
+      })
 
 
     })
