6b7f23c29541c55827899e1d3d0d670b2a082f0e
cyrus <cyrus7@hotmail.fr>
2019-05-06 16:41:55 -0400

    changing framework for reddit

--- code/main_project/server.js
+++ code/main_project/server.js
@@ -5,21 +5,28 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const axios = require("axios");
-const { CommentStream } = require("snoostorm");
-const Snoowrap = require('snoowrap');
-const Snoostorm = require('snoostorm');
+var Snooper = require('reddit-snooper')
 var config = require('./config.js');
 
+snooper = new Snooper(
+        {
+            username: config.reddit_username,
+            password: config.reddit_password,
+            app_id: config.reddit_client_id,
+            api_secret: config.reddit_client_secret,
+            user_agent: 'franklin_ford',
 
-const r = new Snoowrap({
-    userAgent: 'franklin_ford',
-    clientId: config.reddit_client_id,
-    clientSecret: config.reddit_client_secret,
-    username: config.reddit_username,
-    password: config.reddit_password
-});
+            automatic_retries: true, // automatically handles condition when reddit says 'you are doing this too much'
+            api_requests_per_minute: 60 // api requests will be spread out in order to play nicely with Reddit
+        })
 
-const stream = new CommentStream(r, { subreddit: "all", results: 25 });
+
+  snooper.watcher.getPostWatcher("all") // blank argument or 'all' looks at the entire website
+      .on('post', function(post) {
+          console.log('post was posted by: ' + post.data.author)
+          console.log(post)
+      })
+      .on('error', console.error)
 
   app.listen(port, () => {
     console.log('listening on port ' + port)
