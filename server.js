0138f5559d23a10c85cff266debd10cf772d32c1
cyrus <cyrus7@hotmail.fr>
2019-06-24 00:10:56 -0300

    Refactoring master branch

--- server.js
+++ server.js
@@ -252,3 +252,12 @@ var T = new Twit({
   app.listen(port, () => {
     console.log('listening on port ' + port)
   })
+
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
