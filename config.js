4f87950727686814a6a52c39f497d298acf0f7ee
cyrus <cyrus7@hotmail.fr>
2019-06-24 00:03:43 -0300

    Refactoring master branch

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+  preFix: "https://spreadsheets.google.com/feeds/list/",
+  sheetID: "1nSYEF7r33NUQ4aRQQgI1GiGqxFZOKpuj2yhmyyqTMf8",
+  postFix: "/od6/public/values?alt=json",
+  mongoConnectionURL: "mongodb+srv://cyruslk:Sirius321@franklinford-3dveb.mongodb.net/test?retryWrites=true&w=majority",
+  mongoDatabaseName: "franklin-ford-database",
+  watson_app_url: "https://gateway.watsonplatform.net/natural-language-understanding/api",
+  iam_apikey: "GUB8GmVjnAFgY2TAwy_HqeMWy7Zwl-R6js6beazLslT6",
+  reddit_user_agent: "franklin_ford",
+  reddit_client_id: "4NTcUIqouqvcKw",
+  reddit_client_secret: "owbTOIyVQ1nNxLVnV7I6QD7E1cg",
+  reddit_username: "franklin_ford",
+  reddit_password: "U5FfT4jhkb43yNJ",
+  twitter_consumer_key:         '0lCltBgpYXxCfsdn4B59aFvZK',
+  twitter_consumer_secret:      'JvGqFiFHCV4A6YxlqOE8L3LLUuKNHgABOP5ZJJjEClUjDTHaKX',
+  twitter_access_token:         '844002043891240960-FkcSyPk6Bj9BphxnBvWCFJyUxxtA8So',
+  twitter_access_token_secret:  'ma31DbB10AQd80rwJ5xF7In4BErC2NuMq4YjA05CgJ1oT'
+}
+
+module.exports = config;
