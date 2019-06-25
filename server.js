c6aa230bfcc79cf6032114138bcba01a579878bd
cyrus <cyrus7@hotmail.fr>
2019-06-25 18:13:04 -0300

    Heroku

--- server.js
+++ server.js
@@ -252,3 +252,11 @@ var T = new Twit({
   app.listen(port, () => {
     console.log('listening on port ' + port)
   })
+
+
+  if(process.env.NODE_ENV === 'production'){
+      app.use(express.static('client/build'));
+  }
+  app.get('*',(req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
